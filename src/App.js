e3aff1cf86e8e389e23f856904dcf756676e98e8
LarysaBilychenko <116347412+LarysaBilychenko@users.noreply.github.com>
2023-01-25 10:34:18 +0100

    added weather search engine

--- src/App.js
+++ src/App.js
@@ -2,19 +2,60 @@ import axios from "axios";
 import React, { useState } from "react";
 import "./App.css";
 
-function App() {
-  let apiKey = `f454de7ad255eb19e11038486bc33498`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
-  const [temperature, setTemperature] = useState(null);
-  function showTemperature(response) {
-    setTemperature(Math.round(response.data.main.temp));
-  }
-  axios.get(apiUrl).then(showTemperature);
-  return (
-    <div className="App">
-      Current temperature in Kyiv is {temperature} degrees
-    </div>
-  );
-}
+export default function App() {
+  const [city, setCity] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
 
-export default App;
+  function showForecast(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = `f454de7ad255eb19e11038486bc33498`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showForecast);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={updateCity}
+        placeholder="Type your city..."
+        autoComplete="off"
+      />
+      <input type="submit" value="Search" />
+    </form>
+  );
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul className="Forecast">
+          <li>
+            Temperature: {Math.round(weather.temperature)} degrees Celsius;
+          </li>
+          <li>Description: {weather.description}</li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+          <li>Humidity: {Math.round(weather.humidity)}%</li>
+          <li>
+            <img src={weather.icon} alt="weather icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
