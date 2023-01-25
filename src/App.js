334eb2f528367489d698aef16a5c706c175a17c2
LarysaBilychenko <116347412+LarysaBilychenko@users.noreply.github.com>
2023-01-25 09:40:08 +0100

    added axios call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Current temperature in Kyiv is 5 degrees</div>;
+  let apiKey = `f454de7ad255eb19e11038486bc33498`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+  const [temperature, setTemperature] = useState(null);
+  function showTemperature(response) {
+    setTemperature(Math.round(response.data.main.temp));
+  }
+  axios.get(apiUrl).then(showTemperature);
+  return (
+    <div className="App">
+      Current temperature in Kyiv is {temperature} degrees
+    </div>
+  );
 }
 
 export default App;
